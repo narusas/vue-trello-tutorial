e5c1a7dc8894e7606c3a08d940ad685b158d4f40
Jisung, Ahn <narusas@gmail.com>
2021-05-27 09:19:14 +0900

    Lane Title 수정 기능 추가

--- src/components/TasksLane.vue
+++ src/components/TasksLane.vue
@@ -1,72 +1,110 @@
 <template>
   <div class="rounded bg-blue-200 w-64 p-2 mx-2 shadow-2xl">
     <div class="flex justify-between py-1">
-      <h3 class="text-sm font-bold">{{lane.title}}</h3>
+      <h3
+          v-if="isEditingTitle==false"
+          class="text-sm font-bold"
+          @dblclick="goingToEditingTitle"
+      >{{ lane.title }}</h3>
+      <input type="text"
+             v-else
+             class="text-sm font-bold w-full" v-model="editingTitle"
+             @keyup.esc="cancelEditingTitle"
+             @keyup.enter="completeEditingTitle"
+             @blur="completeEditingTitle"
+             ref="editingTitleEl"
+      >
+
     </div>
     <div class="text-sm mt-2">
-        <draggable 
-            group="people"
-            :list="todos"
-            item-key="id"  
-            @change="log"
-        >
-            <template #item="{element}">
-                <task-item :task="element"
-                    @task-title-changed="titleChanged(element, $event)" />
-            </template>
+      <draggable
+          group="people"
+          :list="todos"
+          item-key="id"
+          @change="log"
+      >
+        <template #item="{element}">
+          <task-item :task="element"
+                     @task-title-changed="titleChanged(element, $event)"/>
+        </template>
 
-        </draggable>
-      <p class="mt-3 text-grey-dark flex" @click="addTask"><plus/>Add a card...</p>
+      </draggable>
+      <p class="mt-3 text-grey-dark flex" @click="addTask">
+        <plus/>
+        Add a card...
+      </p>
     </div>
   </div>
 </template>
 <script setup>
-import {ref, defineProps, toRefs, computed, watch} from "vue";
+import {ref, defineProps, toRefs, computed, watch, nextTick} from "vue";
 import {Plus} from "@icon-park/vue-next";
 import TaskItem from "./TaskItem.vue";
 import draggable from '../../node_modules/vuedraggable/src/vuedraggable';
 
 function log(p1) {
-    console.log(p1);
+  console.log(p1);
 }
 
 const props = defineProps({
-    lane: {
-        type: Object
-    }
+  lane: {
+    type: Object
+  }
 });
 
 const a = {
-    age:12,
-    name: "John"
+  age: 12,
+  name: "John"
 };
 
 
 const {lane} = toRefs(props);
 
 console.log("Lane", lane)
+
 function titleChanged(task, changedTitle) {
-    task.title = changedTitle;
+  task.title = changedTitle;
 }
+
 let id = 100;
+
 function addTask() {
-    const newTask = {
-        id: id++,
-        title:'',
-        done:false,
-        isNewAppened: true
-    }
-    todos.value.push(newTask);
+  const newTask = {
+    id: id++,
+    title: '',
+    done: false,
+    isNewAppened: true
+  }
+  todos.value.push(newTask);
 }
-const todos = ref(lane.value.tasks.filter(task => !task.done ))
 
-// watch(()=>[...todos.value], (newValue, oldValue)=>{
-//     console.log(newValue,oldValue);
-// });
+const todos = ref(lane.value.tasks.filter(task => !task.done))
 
+const isEditingTitle = ref(false);
+const editingTitle = ref(null);
+const editingTitleEl = ref(null);
 
-// const todos = computed(()=>{
-//     return lane.value.tasks.filter(task => !task.done );
-// });
+function goingToEditingTitle() {
+  editingTitle.value = lane.value.title;
+  isEditingTitle.value = true;
+  nextTick(() => {
+    !!editingTitleEl.value && editingTitleEl.value.focus();
+  })
+}
+
+function completeEditingTitle() {
+  if (!!editingTitle.value) {
+    lane.value.title = editingTitle.value;
+  }
+  editingTitle.value = null;
+
+  isEditingTitle.value = false;
+}
+
+function cancelEditingTitle() {
+  isEditingTitle.value = false;
+  editingTitle.value = null;
+}
+
 
 </script>

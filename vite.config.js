17f1952464f636c7ef112bb38321e1633e868efe
Jisung, Ahn <narusas@gmail.com>
2021-05-28 15:09:24 +0900

    day5

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target:"http://localhost:3001",
+        changeOrigin: true
+      }
+    }
+  },
   plugins: [vue()],
 });

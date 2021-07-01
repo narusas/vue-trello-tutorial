041d1c917b96458ebaa5671c4634ee39da5e32af
Jisung, Ahn <narusas@gmail.com>
2021-07-01 15:10:28 +0900

    reset

--- src/models/tasks-lanes.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function updateLaneTitle(laneId, title){
-    console.log("updateLaneTitle", laneId, title);
-    await axios.put(`/api/lanes/${laneId}`, {title});
-    await getTasks();
-}
-
--- src/models/tasks-model.js
+++
@@ -1,5 +0,0 @@
-import {ref} from "vue";
-
-export const laneList = ref([]);
-export const lanes = ref(null);
-export const tasks = ref(null);
